1811c06659bb5b0fb15eed5884054bbcf4a88086
Jakub Narloch <jmnarloch@gmail.com>
2015-08-14 21:00:18 +0200

    Additional generator assertions

--- test/test-app.js
+++ test/test-app.js
@@ -9,16 +9,34 @@ describe('scalajs-angular:app', function () {
   before(function (done) {
     helpers.run(path.join(__dirname, '../generators/app'))
       .withOptions({ skipInstall: true })
-      .withPrompts({ someOption: true })
       .on('end', done);
   });
 
   it('creates files', function () {
     assert.file([
-      'bower.json',
-      'package.json',
-      '.editorconfig',
-      '.jshintrc'
+      '.gitignore',
+      'project/build.properties',
+      'project/Build.scala',
+      'project/plugins.sbt',
+      'scalajs/src/main/scala/todomvc/example/Directives.scala',
+      'scalajs/src/main/scala/todomvc/example/Scopes.scala',
+      'scalajs/src/main/scala/todomvc/example/StatusFilter.scala',
+      'scalajs/src/main/scala/todomvc/example/Task.scala',
+      'scalajs/src/main/scala/todomvc/example/TaskService.scala',
+      'scalajs/src/main/scala/todomvc/example/TodoApp.scala',
+      'scalajs/src/main/scala/todomvc/example/TodoCtrl.scala',
+      'scalajvm/app/controllers/SourceMapProvider.scala',
+      'scalajvm/app/controllers/TodoController.scala',
+      'scalajvm/app/todomvc/example/Task.scala',
+      'scalajvm/app/todomvc/example/TaskStore.scala',
+      'scalajvm/app/views/index.scala.html',
+      'scalajvm/app/Application.scala',
+      'scalajvm/conf/evolutions/default/1.sql',
+      'scalajvm/conf/application.conf',
+      'scalajvm/conf/routes',
+      'scalajvm/public/images/bg.png',
+      'scalajvm/public/stylesheets/main.css',
+      'scalajvm/public/templates/todo-item.html'
     ]);
   });
 });
